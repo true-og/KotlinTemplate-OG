44a0805a145e60831d56d48dba22837f30420d2d
NotAlexNoyle <alexnoyle@icloud.com>
2025-06-02 19:27:59 -0400

    Update gradle to 8.14.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.gradleup.shadow") version "8.3.5" // Import shadow API.
+    id("com.gradleup.shadow") version "8.3.6" // Import shadow API.
     java // Tell gradle this is a java project.
     eclipse // Import eclipse plugin for IDE integration.
     kotlin("jvm") version "2.1.21" // Import kotlin jvm plugin for kotlin/java integration.
@@ -25,6 +25,9 @@ tasks.named<ProcessResources>("processResources") {
     filesMatching("plugin.yml") {
         expand(props)
     }
+    from("LICENSE") { // Bundle license into .jars.
+        into("/")
+    }
 }
 
 repositories {
@@ -44,16 +47,13 @@ dependencies {
     compileOnly(project(":libs:DiamondBank-OG"))
 }
 
-tasks.withType<AbstractArchiveTask>().configureEach { // Ensure reproducible builds.
+tasks.withType<AbstractArchiveTask>().configureEach { // Ensure reproducible .jars
     isPreserveFileTimestamps = false
     isReproducibleFileOrder = true
 }
 
 tasks.shadowJar {
-    archiveClassifier.set("") // Use empty string instead of null
-    from("LICENSE") {
-        into("/")
-    }
+    archiveClassifier.set("") // Use empty string instead of null.
     exclude("io.github.miniplaceholders.*") // Exclude the MiniPlaceholders package from being shadowed.
     minimize()
 }
