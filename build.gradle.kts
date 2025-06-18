3c6fb1ad9c305a0b62aaadb362ed1fc8007b885c
NotAlexNoyle <alexnoyle@icloud.com>
2025-06-18 18:36:31 -0400

    Update gradle to 8.14.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "2.1.21" // Import kotlin jvm plugin for kotlin/java integration.
-    id("com.gradleup.shadow") version "8.3.6" // Import shadow API.
     id("com.diffplug.spotless") version "7.0.4"
+    id("com.gradleup.shadow") version "8.3.6" // Import shadow API.
     eclipse // Import eclipse plugin for IDE integration.
 }
 
@@ -11,20 +11,17 @@ java {
 }
 
 group = "net.trueog.kotlintemplate-og" // Declare bundle identifier.
+
 version = "1.0" // Declare plugin version (will be in .jar).
+
 val apiVersion = "1.19" // Declare minecraft server target version.
 
 tasks.named<ProcessResources>("processResources") {
-    val props = mapOf(
-        "version" to version,
-        "apiVersion" to apiVersion
-    )
+    val props = mapOf("version" to version, "apiVersion" to apiVersion)
 
     inputs.properties(props) // Indicates to rerun if version changes.
 
-    filesMatching("plugin.yml") {
-        expand(props)
-    }
+    filesMatching("plugin.yml") { expand(props) }
     from("LICENSE") { // Bundle license into .jars.
         into("/")
     }
@@ -33,9 +30,7 @@ tasks.named<ProcessResources>("processResources") {
 repositories {
     mavenCentral()
     gradlePluginPortal()
-    maven {
-        url = uri("https://repo.purpurmc.org/snapshots")
-    }
+    maven { url = uri("https://repo.purpurmc.org/snapshots") }
 }
 
 dependencies {
@@ -64,9 +59,7 @@ tasks.build {
     dependsOn(tasks.shadowJar)
 }
 
-tasks.jar {
-    archiveClassifier.set("part")
-}
+tasks.jar { archiveClassifier.set("part") }
 
 tasks.withType<JavaCompile>().configureEach {
     options.compilerArgs.add("-parameters")
@@ -75,9 +68,7 @@ tasks.withType<JavaCompile>().configureEach {
     options.isFork = true
 }
 
-kotlin {
-    jvmToolchain(17)
-}
+kotlin { jvmToolchain(17) }
 
 java {
     toolchain {
@@ -87,9 +78,9 @@ java {
 }
 
 spotless {
-    kotlin {
-        ktfmt().kotlinlangStyle().configure {
-            it.setMaxWidth(120)
-        }
+    kotlin { ktfmt().kotlinlangStyle().configure { it.setMaxWidth(120) } }
+    kotlinGradle {
+        ktfmt().kotlinlangStyle().configure { it.setMaxWidth(120) }
+        target("build.gradle.kts", "settings.gradle.kts")
     }
 }
