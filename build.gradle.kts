2d22e701bf1982d3226fab1546c34b0f082099cc
NotAlexNoyle <alexnoyle@icloud.com>
2025-07-30 12:03:36 -0400

    Remove checkstyle (java only)

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
     id("java-library") // Import Java Library plugin.
     id("com.diffplug.spotless") version "7.0.4" // Import Spotless plugin.
     id("com.gradleup.shadow") version "8.3.6" // Import Shadow plugin.
-    id("checkstyle") // Import Checkstyle plugin.
     eclipse // Import Eclipse plugin.
     kotlin("jvm") version "2.1.21" // Import Kotlin JVM plugin.
 }
