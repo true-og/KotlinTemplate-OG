6bf50ac02d266eed2390473ea00dc380f56222fc
NotAlexNoyle <alexnoyle@icloud.com>
2025-12-13 23:20:17 -0500

    Remove extra space

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,6 @@ plugins {
 }
 
 extra["kotlinAttribute"] = Attribute.of("kotlin-tag", Boolean::class.javaObjectType)
-
 val kotlinAttribute: Attribute<Boolean> by rootProject.extra
 
 /* --------------------------- JDK / Kotlin ---------------------------- */
